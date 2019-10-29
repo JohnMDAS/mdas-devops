c328cd4a31c68685c36b0adaa23d13158a8ad6a9
John Sarrià <john@Johns-MacBook-Pro.local>
2019-10-29 21:56:05 +0100

    UpdatedPipeline and added test layer

--- pipeline.sh
+++ pipeline.sh
@@ -1,14 +1,49 @@
 #!/bin/sh
 set -e
+
 go get github.com/labstack/echo
 go get github.com/gorilla/websocket
 
 #cleanup
 
-rm -rf build
+rm -rf build || true
+pkill votingapp || ps aux | grep votingapp | awk {'print $2'} | head -1 | xargs kill -9
+
 #build
 mkdir build
 go build -o ./build ./src/votingapp 
 cp -r ./src/votingapp/ui ./build
-set +e
 
+pushd build
+./votingapp &
+popd
+
+#Test
+test(){
+    curl  --url http://localhost/vote \
+        --request POST \
+        --data '{"topics":["dev", "ops"]}'
+        --header "Content-Type: application/json"
+
+    curl  --url http://localhost/vote \
+        --request PUT \
+        --data '{"topic":"dev"}'
+        --header "Content-Type: application/json"
+
+    winner = $(curl  --url http://localhost/vote \
+        --request DELETE
+        --header "Content-Type: application/json" | jq -r '.winner')
+
+    expectedWinner = "dev"
+
+
+    if [ $expectedWinner == $winner ] then;
+        echo 'TEST PASSED'
+        exit 0
+    else
+        echo 'TEST FAILED'
+        exit 1
+    fi
+
+}
+retry test
