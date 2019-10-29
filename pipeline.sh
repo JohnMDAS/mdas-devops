4f92011ca184700c6462dd10a102e7c1c6370ad8
John Sarrià <john@Johns-MacBook-Pro.local>
2019-10-29 20:41:28 +0100

    Added Pipeline.sh first version

---
+++ pipeline.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+go get github.com/labstack/echo
+go get github.com/gorilla/websocket
+
+#cleanup
+
+rm -rf build
+#build
+mkdir build
+go build -o ./build ./src/votingapp 
+cp -r ./src/votingapp/ui ./build
+set +e
+
