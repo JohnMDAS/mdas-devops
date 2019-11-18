aa2c090b11e24974bcc60f3381f2ba00f01dbacd
John Sarrià <john@Johns-MacBook-Pro.local>
2019-11-18 23:04:33 +0100

    Added First version of python tests.

--- pipeline.sh
+++ pipeline.sh
@@ -1,49 +1,54 @@
 #!/bin/sh
 set -e
 
-go get github.com/labstack/echo
-go get github.com/gorilla/websocket
+#dependencies
+dependencies(){
+    go get github.com/gorilla/websocket
+    go get github.com/labstack/echo
+    go get github.com/go-redis/redis
+
+    pip install requests
+}
 
 #cleanup
-
-rm -rf build || true
-pkill votingapp || ps aux | grep votingapp | awk {'print $2'} | head -1 | xargs kill -9
-
-#build
-mkdir build
-go build -o ./build ./src/votingapp 
-cp -r ./src/votingapp/ui ./build
-
-pushd build
-./votingapp &
-popd
-
-#Test
-test(){
-    curl  --url http://localhost/vote \
-        --request POST \
-        --data '{"topics":["dev", "ops"]}'
-        --header "Content-Type: application/json"
-
-    curl  --url http://localhost/vote \
-        --request PUT \
-        --data '{"topic":"dev"}'
-        --header "Content-Type: application/json"
-
-    winner = $(curl  --url http://localhost/vote \
-        --request DELETE
-        --header "Content-Type: application/json" | jq -r '.winner')
-
-    expectedWinner = "dev"
-
-
-    if [ $expectedWinner == $winner ] then;
-        echo 'TEST PASSED'
-        exit 0
-    else
-        echo 'TEST FAILED'
-        exit 1
-    fi
-
+cleanup(){
+    #pkill votingapp || ps aux | grep votingapp | awk {'print $2'} | head -1 | xargs kill -9 || true
+    rm -rf build
 }
-retry test
+#build
+build()
+{
+    mkdir build
+    go build -o ./build ./src/votingapp 
+    cp -r ./src/votingapp/ui ./build
+
+    pushd build
+    ./votingapp &
+    popd
+}
+
+retry(){
+    n=0
+    interval=5
+    retries=3
+    $@ && return 0
+    until [ $n -ge $retries ]
+    do
+        n=$(($n+1))
+        echo "Retrying...$n of $retries, wait for $interval seconds"
+        sleep $interval
+        $@ && return 0
+    done
+
+    return 1
+}
+
+
+dependencies
+cleanup
+build
+
+pythonOutput=$(python ./pipeline.py 2>&1 > /dev/null)
+echo $pythonOutput
+
+#retry pythonOutput
